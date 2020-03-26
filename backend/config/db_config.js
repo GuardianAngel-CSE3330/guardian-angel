2bbd824f9e6c8ad1f39a213043174a01435f3117
Kyle Mistele <kyle@mistele.com>
2020-03-26 22:36:05 +0000

    updated the db config to use the new credentials in docker-compose.yml

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'localhost',
-    user: 'kyle',
+    host: '172.17.0.1',
+    user: 'root',
     password: 'supersecurepassword',
-    database: 'testdb'
+    database: 'app'
 }
