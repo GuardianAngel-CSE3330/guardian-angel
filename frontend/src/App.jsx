39bfeff3970a87685ab437bcb82ceb54890b94a9
= <nathanhites@yahoo.com>
2020-04-13 13:51:24 -0500

    Begin profile view, use/creation of auth token in 'viewProfile.jsx', cleaned up several other bits of code, added a method to parse jwt tokens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,9 @@ import SafeGuide from './components/pages/safeGuide.jsx';
 import ReportSighting from './components/formsComponent/reportSighting.jsx';
 import ProfileForm from './components/formsComponent/profileForm.jsx';
 import Home from './components/pages/home.jsx';
-import EditProfileForm from './components/formsComponent/editProfile.jsx'
+import EditProfileForm from './components/formsComponent/editProfile.jsx';
+
+import ViewProfile from './components/formsComponent/viewProfile';
 
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
@@ -32,6 +34,8 @@ class App extends Component {
               <Route path="/register" component={ProfileForm} /> 
 
               <Route path ="/safeguide" component = {SafeGuide}/>
+
+              <Route path = "/myprofile" component = {ViewProfile}/>
                
             </Switch>
             
---
+++ frontend/src/components/formsComponent/viewProfile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import parseJwt from '../parsejwt';
+import axios from 'axios';
+
+class ViewProfile extends React.Component {
+
+    profileToken = localStorage.getItem('bearer_token');
+
+    config = {
+        header: 'Bearer '
+    }
+
+    createAuthToken() {
+        debugger;
+        var profileToken = localStorage.getItem('bearer_token');
+        setTimeout(this.config.header = this.config.header.concat(profileToken), 2000);
+    }
+
+    render() {
+        //decode id token
+        this.createAuthToken();
+        var token = this.config.header.substring(7); //substring 7 to remove "Bearer " from token
+        debugger;
+        //get details from decoding id token
+        var params = parseJwt(token);
+        axios.get(`http://localhost:8000/api/private/users/${params.id}`, this.config)
+        .then(
+            //show user profile
+            
+        )
+        .catch(
+        //otherwise (error) display a "you don't have a profile yet"
+        )
+        return <>
+        {/*edit button --> editprofile view */}
+        <button className="btn btn-secondary">Edit Profile</button>
+        </>
+
+    }
+}
+
+export default ViewProfile;
