09d7f682d6a97e4fbd5518474d9d13feaf3e023b
= <nathanhites@yahoo.com>
2020-04-21 20:59:28 -0500

    Added search functionality to search bar, cleaned up auth-token usability (safeGuide). Still need to render search results

--- frontend/src/components/pages/safeGuide.jsx
+++ frontend/src/components/pages/safeGuide.jsx
@@ -10,30 +10,75 @@ class SafeGuide extends Component {
     }
 
     state = {
-
+        searchParams: '',
+        authTokenCreated: false,
+        returnedSightings: []
     }
 
 
+
     async handleButtonPress(){
-        if (localStorage.getItem('bearer_token')) {
+        //If the user has logged in but hasn't created the token yet
+        if (!this.state.authTokenCreated) {
             await this.createAuthToken();
-        } else {
+        //If the user has not logged in yet
+        } else if (this.config.headers.Authorization.localeCompare('Bearer ')) {
             alert("Please log in before searching the database!");
             return;
         }
+        //The user has already logged in + has token when searching
 
 
         if (!this.state.type) {
+            //Search parameter (ghost/location/sighting) wasn't specified
             alert("Please pick a search parameter!")
         }
+        else if (!this.state.searchParams) {
+            //No text was entered in the search box
+            alert("Please enter text before searching!")
+        }
         else if (this.state.type === 'location') {
-            //await axios call
+            //get all sightings, then search through those for a given location
+            await axios.get(`http://localhost:8000/api/private/sightings/all`, this.config)
+            .then(
+                //object given here will be an array
+                sightings => {
+                    debugger;
+                    var allSights = [];
+                    sightings.data.forEach(sighting => {
+                    if(sighting.location.includes(this.state.searchParams)) {
+                        allSights.push(sighting);
+                    }
+                })
+                this.setState({returnedSightings: allSights});
+            }
+            )
         }
         else if (this.state.type === 'ghost') {
-            //await axios call
+            //fetch all sightings of this particular ghost
+            await axios.get(`http://localhost:8000/api/private/sightings/ghost/${this.state.searchParams}`, this.config)
+            .then()
+            .catch(err =>
+                alert(err))
         }
         else if (this.state.type === 'sighting') {
-            //await axios call
+            //get all sightings, then search through titles and descriptions based on search term
+            await axios.get(`http://localhost:8000/api/private/sightings/all`, this.config)
+            .then(
+                //object given here will be an array
+                sightings => {
+                    var allSights = [];
+                    sightings.data.forEach(sighting => {
+                        //if it is in title or description, then push it to the
+                        debugger;
+                        if ((sighting.description.includes(this.state.searchParams || 
+                            sighting.title.includes(this.state.searchParams)))) {
+                                allSights.push(sighting);
+                            }
+                    })
+                this.setState({returnedSightings: allSights});
+                }
+            )
         }
     }
 
@@ -42,13 +87,16 @@ class SafeGuide extends Component {
         var profileToken = await localStorage.getItem('bearer_token');
         console.log('|' + profileToken + '|');
         this.config.headers.Authorization = this.config.headers.Authorization.concat(profileToken);
+        this.setState({authTokenCreated: true});
     }
 
     render(){
         return (
             <div>
                 <div className = "block-example border border-dark m-2">
-                    <input type="text" placeholder="Search.."></input>
+                        {/* Capture text/parameter that is given from user*/}
+                    <input type="text" placeholder="Search.." value={this.state.searchParams}
+                    onChange={e => this.setState({searchParams: e.target.value})}></input>
                     <hr></hr>
                     <input type="radio" id="locSearch" 
                     name="searchType" value="locSearch" onClick={() => this.setState({type: 'location'})}></input>
@@ -63,44 +111,18 @@ class SafeGuide extends Component {
                         <label htmlFor="sigSearch">By Sighting:  </label>
                     <button type="button" className="btn btn-primary" 
                     onClick={() => this.handleButtonPress()}>Search the Database</button>
-                    </div>
+                </div>
 
 
                     <div className = "block-example border border-dark m-2">
-                        <p>
-                            SAFEGUIDE TO DO
-
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when a
-                            n unknown printer took a galley of type and scrambled it to make a type specimen book. It 
-                            has survived not only five centuries, but also the leap into electronic typesetting, remaining es
-                            sentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing L
-                            orem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including ver
-                            sions of Lorem Ipsum.
-                        </p>
-                        <p>
-                        
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when a
-                            n unknown printer took a galley of type and scrambled it to make a type specimen book. It 
-                            has survived not only five centuries, but also the leap into electronic typesetting, remaining es
-                            sentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing L
-                            orem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including ver
-                            sions of Lorem Ipsum.
-                        </p>
-                        <p>
-                        
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when a
-                            n unknown printer took a galley of type and scrambled it to make a type specimen book. It 
-                            has survived not only five centuries, but also the leap into electronic typesetting, remaining es
-                            sentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing L
-                            orem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including ver
-                            sions of Lorem Ipsum.
-                        </p>
+                       <h4 className=""></h4>
                     </div>
                 </div>
     );
+    }
+
+    componentDidUpdate() {
+        //reset state here after the stuff is rendered
     }
 }
 
